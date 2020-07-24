6940352afe94be763d62269e7c26a7b973b4fa75
Maksud Mansuri <uniqueupgradevddr@gmail.com>
2020-07-24 20:04:55 +0530

    rest api2

---
+++ staticfiles/student_lms/assets/js/summernote.js
@@ -0,0 +1,10 @@
+This XML file does not appear to have any style information associated with it. The document tree is shown below.
+<Error>
+<Code>NoSuchKey</Code>
+<Message>The specified key does not exist.</Message>
+<Key>assets/js/summernote.js</Key>
+<RequestId>587E02EBC60A141B</RequestId>
+<HostId>
+oZRQEICEKzl4bm3KwOo7bP9VNoMBeRopdQUkL5Uwm7Aygltmv/mjOpV1Augs3rIBDGfzf58D8TI=
+</HostId>
+</Error>
