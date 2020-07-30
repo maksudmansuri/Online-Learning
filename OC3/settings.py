277f5a5b9c524e2082f9b498bcd976ae9699cdbe
Maksud Mansuri <uniqueupgradevddr@gmail.com>
2020-07-30 17:19:41 +0530

    static file changed

--- OC3/settings.py
+++ OC3/settings.py
@@ -213,11 +213,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
-#Manualy Added
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+# Manualy Added
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
